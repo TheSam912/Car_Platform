a4ddbe2734625bbf68501bf26adb53e77c75031b
“sobhan”912 <sobhan912@gmail.com>
2024-06-23 18:04:26 +0200

    webview updated

--- lib/Utils/webview.dart
+++ lib/Utils/webview.dart
@@ -2,6 +2,8 @@ import 'package:car_platform/Constant/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+import 'navigagion_control.dart';
+
 class myWebview extends StatefulWidget {
   myWebview(this.url);
 
@@ -18,37 +20,71 @@ class _myWebviewState extends State<myWebview> {
   @override
   void initState() {
     super.initState();
-    // controller = WebViewController()
-    //   ..loadRequest(
-    //     Uri.parse('https://flutter.dev'),
-    //   );
     controller = WebViewController()
-      ..setNavigationDelegate(NavigationDelegate(
-        onPageStarted: (url) {
-          setState(() {
-            loadingPercentage = 0;
-          });
+      ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      }, onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      }, onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      },
+
+          // Keeping track of navigation uisng NavigationDelegate
+          onNavigationRequest: (navigation) {
+        final host = Uri.parse(navigation.url).host;
+        if (host.contains('youtube.com')) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                'Blocking navigation to $host',
+              ),
+            ),
+          );
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      }))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'SnackBar',
+        onMessageReceived: (message) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+            message.message,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )));
         },
-        onProgress: (progress) {
-          setState(() {
-            loadingPercentage = progress;
-          });
-        },
-        onPageFinished: (url) {
-          setState(() {
-            loadingPercentage = 100;
-          });
-        },
-      ))
+      )
       ..loadRequest(
-        Uri.parse('https://pub.dev'),
+        Uri.parse('https://flutter.dev'),
       );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mainColor,
+      backgroundColor: Colors.transparent,
+      appBar: PreferredSize(
+          preferredSize: Size(MediaQuery.of(context).size.width, 40),
+          child: Container(
+            color: mainColor,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(top: 50, left: 16),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                )),
+          )),
       body: SafeArea(
         bottom: false,
         child: Stack(
@@ -61,7 +97,19 @@ class _myWebviewState extends State<myWebview> {
                     color: Colors.red,
                     value: loadingPercentage / 100.0,
                   )
-                : Container()
+                : Container(),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 70,
+                  decoration: BoxDecoration(color: mainColor),
+                  child: Center(
+                    child: NavigationControl(controller: controller),
+                  ),
+                ))
           ],
         ),
       ),
--- lib/Widgets/grage_grid_item.dart
+++ lib/Widgets/grage_grid_item.dart
@@ -2,45 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Widget garageGridItem(text, icon) {
-  return Expanded(
-    child: Container(
-      height: 120,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.white.withOpacity(0.1),
-            offset: Offset(-4.0, -4.0),
-            blurRadius: 16.0,
-          ),
-          BoxShadow(
-            color: Colors.black.withOpacity(0.4),
-            offset: Offset(6.0, 6.0),
-            blurRadius: 16.0,
-          ),
-        ],
-        color: Color(0xFF292D32),
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            text,
-            style: GoogleFonts.montserrat(
-                color: Colors.green, fontWeight: FontWeight.w500, fontSize: 11),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Image(
-            image: AssetImage(icon),
-            width: 28,
-            height: 28,
-            color: Colors.green,
-          )
-        ],
-      ),
+  return Container(
+    height: 120,
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      boxShadow: [
+        BoxShadow(
+          color: Colors.white.withOpacity(0.1),
+          offset: Offset(-4.0, -4.0),
+          blurRadius: 16.0,
+        ),
+        BoxShadow(
+          color: Colors.black.withOpacity(0.4),
+          offset: Offset(6.0, 6.0),
+          blurRadius: 16.0,
+        ),
+      ],
+      color: Color(0xFF292D32),
+      borderRadius: BorderRadius.circular(12.0),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          text,
+          style: GoogleFonts.montserrat(
+              color: Colors.green, fontWeight: FontWeight.w500, fontSize: 11),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Image(
+          image: AssetImage(icon),
+          width: 28,
+          height: 28,
+          color: Colors.green,
+        )
+      ],
     ),
   );
 }
